96ff2f86a9f77418f7c2138535c86b52ce796321
Giorgi Lomidze <lomiddze@gmail.com>
2026-05-29 21:48:13 +0400

    Reduce Calendly widget height to remove unnecessary scrollbar

--- script.js
+++ script.js
@@ -1185,7 +1185,7 @@
       });
       widget.setAttribute('data-url', `${CALENDLY_URL}?${params.toString()}`);
       widget.style.minWidth = '320px';
-      widget.style.height = '780px';
+      widget.style.height = '630px';
       placeholder.appendChild(widget);
 
       // Inject Calendly's external script once
